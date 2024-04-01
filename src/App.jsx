d80b240c200162c68a275bca6bfaaf891f6953c3
zk2k2 <ziedkharrat4juin@gmail.com>
2024-04-01 18:08:56 +0100

    added Dockerfile and removed unused css

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import { Routes, Route } from "react-router-dom";
 import { BrowserRouter as Router } from "react-router-dom";
 
 // stylesheet
-import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.min.js";
 
